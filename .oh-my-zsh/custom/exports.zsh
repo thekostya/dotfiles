a92a90c4f9510b87f736198f69c8011368d223f4
Konstantin Ryabov <thekostya@gmail.com>
2018-08-16 15:20:38 +0300

    add ohmyzsh

---
+++ .oh-my-zsh/custom/exports.zsh
@@ -0,0 +1,21 @@
+# Make vim the default editor.
+export EDITOR='vim';
+
+# Increase Bash history size. Allow 32³ entries; the default is 500.
+export HISTSIZE='32768';
+export HISTFILESIZE="${HISTSIZE}";
+# Omit duplicates and commands that begin with a space from history.
+export HISTCONTROL='ignoreboth';
+
+# Prefer US English and use UTF-8.
+export LANG='en_US.UTF-8';
+export LC_ALL='en_US.UTF-8';
+
+# Always enable colored `grep` output.
+export GREP_OPTIONS='--color=auto';
+
+# golang
+export GOROOT="/usr/local/opt/go/libexec"
+export GOPATH="${HOME}/go
+
+
---
+++ .oh-my-zsh/custom/path.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/bin:$PATH"
+export PATH="$HOME/.vim/bin:$PATH"
+export PATH="$GOROOT/bin:$GOPATH/bin:$PATH";
+export PATH="$HOME/bin:$PATH"
+
